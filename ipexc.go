df62c25beda6a5de63120ad557e0c569be0013bc
Maciej <maciej@contact.apurer.io>
2021-02-01 00:16:52 +0100

    changed functions input types

--- ipexc.go
+++ ipexc.go
@@ -1,29 +1,13 @@
 package ipexc
 
 import (
-	"encoding/binary"
-	"net"
 	"os/exec"
-	"strconv"
 )
 
-func ipToint(ip net.IP) uint32 {
-	if len(ip) == 16 {
-		return binary.BigEndian.Uint32(ip[12:16])
-	}
-	return binary.BigEndian.Uint32(ip)
-}
-
-func intToip(nn uint32) net.IP {
-	ip := make(net.IP, 4)
-	binary.BigEndian.PutUint32(ip, nn)
-	return ip
-}
-
 // Insert function for inserting new records into tables INPUT and OUTPUT
-func Insert(port uint64, ip uint32) error {
+func Insert(port string, ip string) error {
 
-	args := []string{"-I", "INPUT", "-p", "tcp", "-s", intToip(ip).String(), "--dport", strconv.FormatUint(port, 10), "-m", "state", "--state", "NEW,ESTABLISHED", "-j", "ACCEPT"}
+	args := []string{"-I", "INPUT", "-p", "tcp", "-s", ip, "--dport", port, "-m", "state", "--state", "NEW,ESTABLISHED", "-j", "ACCEPT"}
 
 	cmd := exec.Command("iptables", args...)
 
@@ -46,9 +30,9 @@ func Insert(port uint64, ip uint32) error {
 }
 
 // Delete function for deleting new records from tables INPUT and OUTPUT
-func Delete(port uint64, ip uint32) error {
+func Delete(port string, ip string) error {
 
-	args := []string{"-D", "INPUT", "-p", "tcp", "-s", intToip(ip).String(), "--dport", strconv.FormatUint(port, 10), "-m", "state", "--state", "NEW,ESTABLISHED", "-j", "ACCEPT"}
+	args := []string{"-D", "INPUT", "-p", "tcp", "-s", ip, "--dport", port, "-m", "state", "--state", "NEW,ESTABLISHED", "-j", "ACCEPT"}
 
 	cmd := exec.Command("iptables", args...)
 
